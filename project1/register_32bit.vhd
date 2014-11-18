96d2d9d8baa789d931f06c3752f406a3c24a40f9
Matthew Albrecht <MatthewAlbrecht2015@gmail.com>
2014-11-17 20:07:13 -0600

    Created project and made 32-bit register

---
+++ project1/register_32bit.vhd
@@ -0,0 +1,23 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.eecs361_gates.all;
+use work.eecs361.all;
+
+entity register_32bit is
+    port (
+        clock : in std_logic;
+        write_en: in std_logic;
+        D   : in std_logic_vector(31 downto 0);
+        Z   : out std_logic_vector(31 downto 0)
+    );
+end register_32bit;
+
+architecture structural of register_32bit is
+    signal ored_clock : std_logic;
+begin
+    or_clk : or_gate port map(x=>clock,y=>write_en,z=>ored_clock);
+    gen_ffs:
+    for I in 0 to 31 generate
+       dffx: dff port map (clk=>ored_clock,d=>D(I),q=>Z(I));
+   end generate gen_ffs;
+end architecture structural;
