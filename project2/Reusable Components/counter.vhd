ea7f506592662b44f3b91214de211f883f5453ee
Matthew Albrecht <MatthewAlbrecht2015@gmail.com>
2014-12-10 15:12:14 -0600

    Made Counter have max value

--- project2/Reusable Components/counter.vhd
+++ project2/Reusable Components/counter.vhd
@@ -5,31 +5,41 @@ use work.eecs361.all;
 
 entity counter is
 	port (
-		inc		: in std_logic;
-		clk		: in std_logic;
-		reset	: in std_logic;
-		cnt		: out std_logic_vector(31 downto 0)
+		WriteEnable		: in std_logic;
+		Clk		: in std_logic;
+		Reset	: in std_logic;
+		HighestValue : in std_logic_vector(31 downto 0);
+		Count		: out std_logic_vector(31 downto 0)
 	);
 end counter;
 
 architecture structural of counter is
-	signal resetLow		: std_logic;
-	signal registerIn	: std_logic_vector(31 downto 0);
-	signal registerOut	: std_logic_vector(31 downto 0);
-
+	signal ResetLow			: std_logic;
+	signal registerInNext	: std_logic_vector(31 downto 0);
+	signal registerOut		: std_logic_vector(31 downto 0);
+	signal registerIn 		: std_logic_vector(31 downto 0);
+	signal notRegisterOut 	: std_logic_vector(31 downto 0);
+	signal maxedOut : std_logic;
 begin
 
-	--Not the reset so the counter is reset on high
-	not1 : not_gate port map(reset, resetLow);
+	--Not the Reset so the counter is Reset on high
+	not1 : not_gate port map(Reset, ResetLow);
 
 	-- Register countaining count
-	reg : register_32bit port map(clk => clk, reset_active_low => resetLow, write_en => inc, D => registerIn, Z => registerOut);
+	reg : register_32bit port map(Clk => Clk, Reset_active_low => ResetLow, write_en => WriteEnable, D => registerIn, Z => registerOut);
 
 	--Add 1 to the register output
-	add : fulladder_32 port map(cin => '1', x => registerOut, y => (31 downto 0 => '0'), cout => open, z => registerIn);
+	add : fulladder_32 port map(cin => '1', x => registerOut, y => (31 downto 0 => '0'), cout => open, z => registerInNext);
 
+	compareWithHighestValue : cmp_n port map(a=>registerOut,
+									b=>HighestValue,a_eq_b=>maxedOut);
+
+	ResetIfMaxed : mux_n generic map(n=>32) port map(sel=>maxedOut,
+									src0=>registerInNext,
+									src1=>x"00000000",
+									z=>registerIn);
 	--output current count
-	cnt <= registerOut;
+	Count <= registerOut;
 
 
 end;
